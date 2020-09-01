f2b49890102644f7f1d54a8f74f54e36e02133de
McCoy Zhu <mz1945@nyu.edu>
2020-09-01 14:04:12 -0700

    Successfully parsed yaml and ran the first test

--- BackstopJS Test/parse_yaml.js
+++ BackstopJS Test/parse_yaml.js
@@ -1,7 +1,66 @@
 const YAML = require('yaml');
 const fs = require('fs');
+const backstop = require('backstopjs');
 
 const file = fs.readFileSync('nyu.yml', 'utf8');
 const parsed = YAML.parse(file);
 
-console.log(parsed);
+const scenarios = parsed.urls;
+
+const defaultConfig = {
+    paths: {
+        bitmaps_reference: "backstop_data/bitmaps_reference",
+        bitmaps_test: "backstop_data/bitmaps_test",
+        engine_scripts: "backstop_data/engine_scripts",
+        html_report: "backstop_data/html_report",
+        ci_report: "backstop_data/ci_report"
+    },
+    report: ["browser"],
+    engine: "puppeteer",
+    engineOptions: {
+        "args": ["--no-sandbox"]
+    },
+    asyncCaptureLimit: 20,
+    asyncCompareLimit: 100,
+    debug: false,
+    debugWindow: false
+}
+
+let scenario = scenarios[0]
+// for (const scenario of scenarios) {
+    let config = Object.assign({}, defaultConfig);
+    config.id = scenario.name;
+
+    config.viewports = scenario["screen_sizes"].map((screenSizeStr) => {
+        const match = screenSizeStr.match(/([1-9][0-9]{0,})x([1-9][0-9]{0,})/);
+        return {
+            label: screenSizeStr,
+            width: parseInt(match[1]),
+            height: parseInt(match[2])
+        }
+    });
+
+    config.scenarios = [
+        {
+            label: scenario.name,
+            cookiePath: "",
+            url: scenario.url1,
+            referenceUrl: scenario.url2,
+            readyEvent: "",
+            readySelector: "",
+            delay: parseInt(scenario.delay || "0") === 0 ? 0 : 3000,
+            hideSelectors: [],
+            removeSelectors: [],
+            hoverSelector: "",
+            clickSelector: "",
+            postInteractionWait: 0,
+            selectors: [],
+            selectorExpansion: true,
+            expect: 0,
+            misMatchThreshold : 0.1,
+            requireSameDimensions: true
+        }
+    ]
+
+    backstop("test", {config: config});
+// }
