374bcedbb8dd0e4fcf28f72309a3e00f6cf38074
McCoy Zhu <mz1945@nyu.edu>
2020-10-01 14:13:46 -0700

    Testing with child_process and exec

--- switch-browser.js
+++ switch-browser.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
+const { exec } = require('child_process');
 
 const helper = require("./helper")
 // const rl = helper.rl();
 const logStyle = helper.logStyle;
 
 const firefoxKey = "PUPPETEER_PRODUCT=firefox";
-const installAllCmd = "npm install";
-const installPuppeteerCmd = "npm install puppeteer";
+const installCmd = "npm install";
 
 /**
  * Current product of the installed puppeteer.
@@ -16,7 +16,7 @@ const installPuppeteerCmd = "npm install puppeteer";
 let currentPuppeteerProduct = helper.puppeteerProduct();
 
 /**
- * Accesses package.json in backstopjs and takes a given action.
+ * Accesses "package.json" in backstopjs and takes a given action.
  *
  * @param {function} action The action to take should package.json be found and parsed
  * @return {void}
@@ -46,14 +46,14 @@ function accessPackageJson(action) {
 }
 
 /**
- * Writes to package.json in backstopjs.
+ * Writes to "package.json" in backstopjs.
  *
  * @param {Object} packageJson Package to write to package.json
  * @param {function} handler Runs after write succeeded
  * @return {void}
  */
 function writeToPackageJson(packageJson, handler = () => {}) {
-    fs.writeFile("node_modules/backstopjs/package1.json", JSON.stringify(packageJson, null, 2), err => {
+    fs.writeFile("node_modules/backstopjs/package.json", JSON.stringify(packageJson, null, 2), err => {
         if (err) {
             console.error(`${logStyle.fg.red}File "node_modules/backstopjs/package.json" cannot be saved: ${err}${logStyle.reset}`);
             process.exit(1);
@@ -64,14 +64,78 @@ function writeToPackageJson(packageJson, handler = () => {}) {
     });
 }
 
+/**
+ * Runs a terminal shell command.
+ *
+ * @param {string} cmd Command to run
+ * @param {function} handler Runs after write succeeded
+ * @return {void}
+ */
+function execute(cmd, handler = () => {}) {
+    exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`${logStyle.fg.red}${error}${logStyle.reset}`);
+            process.exit(1);
+        }
+
+        if (stdout) {
+            console.log(stdout);
+        }
+
+        if (stderr) {
+            console.warn(stderr);
+        }
+
+        handler();
+    });
+}
+
+/**
+ * Uninstalls Puppeteer by removing it from "package.json" and calling "npm install".
+ *
+ * @return {void}
+ */
+function uninstallPuppeteer() {
+    accessPackageJson((packageJson) => {
+        delete packageJson["dependencies"]["puppeteer"];
+        console.log(`${logStyle.fg.white}Removing "puppeteer" from "dependencies"${logStyle.reset}`);
+
+        writeToPackageJson(packageJson, () => {
+            execute(`${installCmd}`, () => {
+                console.log(`${logStyle.fg.green}success${logStyle.reset}`);
+            });
+        });
+    });
+}
+
+/**
+ * Installs Puppeteer by adding it to "package.json" and calling "npm install".
+ *
+ * @param {string} product Puppeteer product to install
+ * @return {void}
+ */
+function installPuppeteer(product) {
+    accessPackageJson((packageJson) => {
+        packageJson["dependencies"]["puppeteer"] = "^5.3.0";
+        console.log(`${logStyle.fg.white}Setting "puppeteer" to "^5.3.0" in "dependencies"${logStyle.reset}`);
+
+        writeToPackageJson(packageJson, () => {
+            execute(product === "firefox" ? `${firefoxKey} ${installCmd}` : `${installCmd}`, () => {
+                console.log(`${logStyle.fg.green}success${logStyle.reset}`);
+            });
+        });
+    });
+}
+
+
 // accessPackageJson((packageJson) => {
 //     packageJson["dependencies"]["puppeteer"] = "^5.3.0";
 //     console.log(`${logStyle.fg.white}Setting "puppeteer" to "^5.3.0" in "dependencies"${logStyle.reset}`);
 //     writeToPackageJson(packageJson);
 // });
 
-accessPackageJson((packageJson) => {
-    delete packageJson["dependencies"]["puppeteer"];
-    console.log(`${logStyle.fg.white}Removing "puppeteer" from "dependencies"${logStyle.reset}`);
-    writeToPackageJson(packageJson);
-});
+// accessPackageJson((packageJson) => {
+//     delete packageJson["dependencies"]["puppeteer"];
+//     console.log(`${logStyle.fg.white}Removing "puppeteer" from "dependencies"${logStyle.reset}`);
+//     writeToPackageJson(packageJson);
+// });
