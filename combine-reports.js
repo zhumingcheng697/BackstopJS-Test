f91949145cfa18d31a9e895b95b19202e02fee3f
McCoy Zhu <mz1945@nyu.edu>
2020-12-10 19:35:47 -0800

    Testing config.js parsing

---
+++ combine-reports.js
@@ -0,0 +1,148 @@
+const str = `
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../../../bitmaps_reference/webkit/Alumni_Content/01781168764_Alumni_Content_0_document_0_320x2500_webkit.png",
+        "test": "../../../bitmaps_test/webkit/Alumni_Content/20201123-102921/01781168764_Alumni_Content_0_document_0_320x2500_webkit.png",
+        "selector": "document",
+        "fileName": "01781168764_Alumni_Content_0_document_0_320x2500_webkit.png",
+        "label": "Alumni_Content",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://qacms.home.nyu.edu/employees/resources-and-services/media-and-communications/digital-communications/test/qa-regression/alumni-role-page/alumni-content-template.html",
+        "referenceUrl": "http://www.nyu.edu/employees/resources-and-services/media-and-communications/digital-communications/test/qa-regression/alumni-role-page/alumni-content-template.html",
+        "expect": 0,
+        "viewportLabel": "320x2500",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -83
+          },
+          "misMatchPercentage": "19.33",
+          "analysisTime": 36
+        },
+        "diffImage": "../../../bitmaps_test/webkit/Alumni_Content/20201123-102921/failed_diff_01781168764_Alumni_Content_0_document_0_320x2500_webkit.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../../../bitmaps_reference/webkit/Alumni_Content/01781168764_Alumni_Content_0_document_1_480x2500_webkit.png",
+        "test": "../../../bitmaps_test/webkit/Alumni_Content/20201123-102921/01781168764_Alumni_Content_0_document_1_480x2500_webkit.png",
+        "selector": "document",
+        "fileName": "01781168764_Alumni_Content_0_document_1_480x2500_webkit.png",
+        "label": "Alumni_Content",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://qacms.home.nyu.edu/employees/resources-and-services/media-and-communications/digital-communications/test/qa-regression/alumni-role-page/alumni-content-template.html",
+        "referenceUrl": "http://www.nyu.edu/employees/resources-and-services/media-and-communications/digital-communications/test/qa-regression/alumni-role-page/alumni-content-template.html",
+        "expect": 0,
+        "viewportLabel": "480x2500",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -83
+          },
+          "misMatchPercentage": "18.43",
+          "analysisTime": 44
+        },
+        "diffImage": "../../../bitmaps_test/webkit/Alumni_Content/20201123-102921/failed_diff_01781168764_Alumni_Content_0_document_1_480x2500_webkit.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../../../bitmaps_reference/webkit/Alumni_Content/01781168764_Alumni_Content_0_document_2_690x2500_webkit.png",
+        "test": "../../../bitmaps_test/webkit/Alumni_Content/20201123-102921/01781168764_Alumni_Content_0_document_2_690x2500_webkit.png",
+        "selector": "document",
+        "fileName": "01781168764_Alumni_Content_0_document_2_690x2500_webkit.png",
+        "label": "Alumni_Content",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://qacms.home.nyu.edu/employees/resources-and-services/media-and-communications/digital-communications/test/qa-regression/alumni-role-page/alumni-content-template.html",
+        "referenceUrl": "http://www.nyu.edu/employees/resources-and-services/media-and-communications/digital-communications/test/qa-regression/alumni-role-page/alumni-content-template.html",
+        "expect": 0,
+        "viewportLabel": "690x2500",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -83
+          },
+          "misMatchPercentage": "17.94",
+          "analysisTime": 50
+        },
+        "diffImage": "../../../bitmaps_test/webkit/Alumni_Content/20201123-102921/failed_diff_01781168764_Alumni_Content_0_document_2_690x2500_webkit.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../../../bitmaps_reference/webkit/Alumni_Content/01781168764_Alumni_Content_0_document_3_930x2500_webkit.png",
+        "test": "../../../bitmaps_test/webkit/Alumni_Content/20201123-102921/01781168764_Alumni_Content_0_document_3_930x2500_webkit.png",
+        "selector": "document",
+        "fileName": "01781168764_Alumni_Content_0_document_3_930x2500_webkit.png",
+        "label": "Alumni_Content",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://qacms.home.nyu.edu/employees/resources-and-services/media-and-communications/digital-communications/test/qa-regression/alumni-role-page/alumni-content-template.html",
+        "referenceUrl": "http://www.nyu.edu/employees/resources-and-services/media-and-communications/digital-communications/test/qa-regression/alumni-role-page/alumni-content-template.html",
+        "expect": 0,
+        "viewportLabel": "930x2500",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -98
+          },
+          "misMatchPercentage": "17.74",
+          "analysisTime": 63
+        },
+        "diffImage": "../../../bitmaps_test/webkit/Alumni_Content/20201123-102921/failed_diff_01781168764_Alumni_Content_0_document_3_930x2500_webkit.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../../../bitmaps_reference/webkit/Alumni_Content/01781168764_Alumni_Content_0_document_4_1200x2500_webkit.png",
+        "test": "../../../bitmaps_test/webkit/Alumni_Content/20201123-102921/01781168764_Alumni_Content_0_document_4_1200x2500_webkit.png",
+        "selector": "document",
+        "fileName": "01781168764_Alumni_Content_0_document_4_1200x2500_webkit.png",
+        "label": "Alumni_Content",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://qacms.home.nyu.edu/employees/resources-and-services/media-and-communications/digital-communications/test/qa-regression/alumni-role-page/alumni-content-template.html",
+        "referenceUrl": "http://www.nyu.edu/employees/resources-and-services/media-and-communications/digital-communications/test/qa-regression/alumni-role-page/alumni-content-template.html",
+        "expect": 0,
+        "viewportLabel": "1200x2500",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -98
+          },
+          "misMatchPercentage": "16.51",
+          "analysisTime": 78
+        },
+        "diffImage": "../../../bitmaps_test/webkit/Alumni_Content/20201123-102921/failed_diff_01781168764_Alumni_Content_0_document_4_1200x2500_webkit.png"
+      },
+      "status": "fail"
+    }
+  ]
+});
+`
+
+const leftIndex = str.indexOf("[");
+const rightIndex = str.lastIndexOf("]");
+
+const content = str.slice(leftIndex + 1, rightIndex).replace(/(?:\.\.\/){3}/g, "./backstop_data");
+
+console.log(
+    JSON.parse("[" +
+        [content, content].join(",")
+    + "]")
+)
