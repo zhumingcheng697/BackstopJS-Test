a75dfafbd18f5c1f8741fd63cd58bd9bbd440cba
McCoy Zhu <mz1945@nyu.edu>
2020-12-10 22:52:17 -0800

    iterating through all the possible configs

--- combine-reports.js
+++ combine-reports.js
@@ -1,13 +1,34 @@
-const fs = require("fs")
-const str = fs.readFileSync("backstop_data/html_report/chromium/Alumni_Content/config.js", "utf8")
+const fs = require("fs");
 
-const leftIndex = str.indexOf("[");
-const rightIndex = str.lastIndexOf("]");
+const combinedTests = [];
 
-const content = str.slice(leftIndex + 1, rightIndex).replace(/(?:\.\.\/){3}/g, "backstop_data");
+/**
+ * Extracts tests from config
+ *
+ * @param config {string}
+ * @returns {string}
+ */
+function extractedTests(config) {
+    const leftIndex = config.indexOf("[");
+    const rightIndex = config.lastIndexOf("]");
 
-console.log(
-    JSON.parse("[" +
-        [content, content].join(",")
-    + "]")
-)
+    const allTests = config.slice(leftIndex + 1, rightIndex).replace(/(?:\.\.\/){3}/g, "../../backstop_data");
+    return "    {" + allTests.split(/}[\n\s]*,[\n\s]*{/).filter((test) => test.includes(`"status": "fail"`)).join(`},\n{`) + "}";
+}
+
+for (const browserType of ["chromium", "firefox", "webkit"]) {
+    const pathForBrowser = `backstop_data/html_report/${browserType}`
+    if (fs.existsSync(pathForBrowser)) {
+        for (const dir of fs.readdirSync(pathForBrowser, { withFileTypes: true })) {
+            if (dir.isDirectory()) {
+                const pathForConfig = `${pathForBrowser}/${dir.name}/config.js`;
+
+                if (fs.existsSync(pathForConfig)) {
+                    const config = fs.readFileSync(pathForConfig, "utf8");
+                    const tests = extractedTests(config);
+                    combinedTests.push(tests)
+                }
+            }
+        }
+    }
+}
