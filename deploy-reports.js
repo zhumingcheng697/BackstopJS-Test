397af63e23e7065b90fd158e4d5e1cfeeefdfa87
McCoy Zhu <mz1945@nyu.edu>
2021-02-04 14:55:02 -0800

    ADD: check if bucket exists

--- deploy-reports.js
+++ deploy-reports.js
@@ -1,12 +1,25 @@
-const AWS = require("aws-sdk");
-AWS.config.update({region: "us-east-1"});
+const logRed = "\x1b[31m";
+const logGreen = "\x1b[32m";
+const logReset = "\x1b[0m";
+const bucketName = process.argv[2] || "backstop-reports";
 
-const s3 = new AWS.S3({apiVersion: "2006-03-01"});
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "us-east-1" });
+
+const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+
 s3.listBuckets((err, data) => {
     if (err) {
-        console.log("Error", require("util").inspect(err, false, null));
+        console.error(`${logRed}An error occurred when trying to connect to AWS:\n${err}${logReset}`);
+        process.exit(1);
     } else {
-        console.log("Success", data.Buckets);
-        console.log("Success", require("util").inspect(data.Buckets, false, null));
+        if (data.Buckets.find((bucket) => (bucket.Name === bucketName))) {
+            console.log(`${logGreen}Bucket "${bucketName}" found${logReset}`);
+            // upload files
+        } else {
+            console.error(`${logRed}Bucket "${bucketName}" does not exist${logReset}`);
+            console.log(`Creating bucket "${bucketName}"`);
+            // create bucket, then upload files
+        }
     }
 });
