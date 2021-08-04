4ab22a4fa2d19bc57cb9c99233846294ad0a9795
McCoy Zhu <mz1945@nyu.edu>
2021-08-03 18:04:24 -0700

    ADD: ubuntu-webkit-fix.bash

---
+++ ubuntu-webkit-fix.bash
@@ -0,0 +1,3 @@
+mkdir -p /etc/gnutls
+echo "[overrides]" >> /etc/gnutls/config
+echo "default-priority-string = NORMAL:-VERS-ALL:+VERS-TLS1.3:+VERS-TLS1.2:+VERS-DTLS1.2:%PROFILE_LOW" >> /etc/gnutls/config
